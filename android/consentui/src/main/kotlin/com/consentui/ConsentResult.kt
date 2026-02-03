e9467b482d4f5fb2a55332740ab286d1a741c9cd
Wondae <48678910+Wondae-code@users.noreply.github.com>
2026-02-03 15:45:18 +0900

    add ConsentResultType enum for Java interoperability

--- android/consentui/src/main/kotlin/com/consentui/ConsentResult.kt
+++ android/consentui/src/main/kotlin/com/consentui/ConsentResult.kt
@@ -1,17 +1,49 @@
 package com.consentui
 
 /**
+ * Enum representation of consent result types for Java interoperability.
+ * Use this enum in Java switch statements for easier handling.
+ */
+enum class ConsentResultType {
+    ACCEPTED,
+    DECLINED,
+    NOT_REQUIRED
+}
+
+/**
  * Result of the consent request
  */
 sealed class ConsentResult {
+    /** The type of this result as an enum for Java interoperability */
+    abstract val type: ConsentResultType
+
     /** User accepted the consent request */
-    object Accepted : ConsentResult()
+    object Accepted : ConsentResult() {
+        override val type: ConsentResultType = ConsentResultType.ACCEPTED
+    }
 
     /** User declined the consent request */
-    object Declined : ConsentResult()
+    object Declined : ConsentResult() {
+        override val type: ConsentResultType = ConsentResultType.DECLINED
+    }
 
     /** Consent was not required (user not in EEA region) */
-    object NotRequired : ConsentResult()
+    object NotRequired : ConsentResult() {
+        override val type: ConsentResultType = ConsentResultType.NOT_REQUIRED
+    }
+
+    companion object {
+        /**
+         * Creates a ConsentResult from the given type enum.
+         * Useful for Java code that needs to convert from enum to sealed class.
+         */
+        @JvmStatic
+        fun fromType(type: ConsentResultType): ConsentResult = when (type) {
+            ConsentResultType.ACCEPTED -> Accepted
+            ConsentResultType.DECLINED -> Declined
+            ConsentResultType.NOT_REQUIRED -> NotRequired
+        }
+    }
 }
 
 /**
